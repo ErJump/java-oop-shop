6f823ddb6db3f6a79df11861ea71eeec5a76611f
ErJump <fedelijunior@gmail.com>
2022-11-17 14:26:02 +0100

    feat: :sparkles: Adds class Prodotto

---
+++ src/generation/italy/shop/Prodotto.java
@@ -0,0 +1,33 @@
+package generation.italy.shop;
+
+import java.util.Random;
+
+public class Prodotto {
+	int codice;
+	String nome;
+	String descrizione;
+	int prezzo;
+	int iva = 20;
+	
+	Prodotto(String nome, String descrizione, int prezzo) {
+		Random rnd = new Random();
+		this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.codice = rnd.nextInt(99999);
+	}
+
+    public int getPrice() {
+        return prezzo;
+    }
+
+    public int getPriceIVA() {
+        return prezzo + (prezzo * iva / 100);
+    }
+    
+    @Override
+    public String toString() {
+    	return "Nome: " + nome + "\nDescrizione: " + descrizione + "\nCodice: " + codice + "\nPrezzo: " + prezzo ;
+    }
+    
+}
